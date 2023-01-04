d439b980f730e9955d1dacca6aa3d24048c6be6f
ryojio2k <ryojio2k@gmail.com>
2023-01-04 14:06:24 +0900

    関数名の命名規則に則った修正

--- GcdRandomRepeat.py
+++ GcdRandomRepeat.py
@@ -2,7 +2,7 @@ import sys
 import random
 import Gcd
 
-def gcd_RandomRepeat(r = 10):
+def gcd_random_repeat(r = 10):
   """最大公約数を繰り返し算出する
 
   ランダムで選出した2つの整数に対し、最大公約数を求める
@@ -19,6 +19,6 @@ def gcd_RandomRepeat(r = 10):
 if __name__ == "__main__":
   args = sys.argv
   if len(args) < 2:
-    gcd_RandomRepeat()
+    gcd_random_repeat()
   else:
-    gcd_RandomRepeat(int(args[1]))
+    gcd_random_repeat(int(args[1]))
